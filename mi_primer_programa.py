f2e04b2cb1bcfd6aba18c37dfc1912333def6d3f
aleemejia901 <aleemejia901@gmail.com>
2026-05-10 17:50:16 -0500

    Create mi_primer_programa.py
    Mi primer programa con claude

---
+++ mi_primer_programa.py
@@ -0,0 +1,7 @@
+nombre = input("¿Cómo te llamas? ")
+edad = int(input("¿Cuántos años tienes? "))
+años_para_uni = 18 - edad
+
+print("Hola", nombre + "!")
+print("Te faltan", años_para_uni, "años para la universidad")
+print("Para entonces ya serás un programador crack")
